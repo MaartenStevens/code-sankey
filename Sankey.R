5861a79927293efeb58502270b84c5c364b17e05
MaartenStevens <maarten.stevens@inbo.be>
2020-01-23 14:59:19 +0100

    Aangepaste code met definitieve opmaak grafiek

--- Sankey.R
+++ Sankey.R
@@ -5,16 +5,38 @@
 #################################################################
 # Packages laden
 
-library(networkD3)
+# devtools::install_github("fbreitwieser/sankeyD3")
+
+# library(networkD3) # Minder keuze voor opmaak diagram
+library(sankeyD3) # Met dit package heb je controle over de font, de opacity, ...
 library(tidyverse)
 
 #################################################################
 # Dataset laden
 
-sank <- read.csv(file = "./data/Sankey.csv", sep=";", dec = ",")
+sank_pre <- read.csv(file = "./data/clc_1990_2018.csv", sep=";", dec = ",")
+code <- read.csv(file = "./data/clc_1990_2018_code.csv", sep=";", dec = ",") %>% 
+  select(c(clc_code, nara5_code, nara5_label, nara5_labels))
 
 #################################################################
 # Data bewerken
+
+# Data groeperen per NARA-klasse
+
+sank <- sank_pre %>%
+  left_join(code, by = c("c1990" = "clc_code")) %>% 
+  rename("from" = "nara5_code") %>%
+  rename("labelS" = "nara5_labels") %>% 
+  left_join(code, by = c("c2018" = "clc_code")) %>% 
+  rename("to" = "nara5_code") %>% 
+  rename("labelT" = "nara5_labels") %>%
+  group_by(from, to) %>% 
+  summarise(value = sum(count),
+            labelS = unique(labelS),
+            labelT = unique(labelT)) %>% 
+  ungroup() %>% 
+  mutate("jaarS" = 1990) %>% 
+  mutate("jaarT" = 2018)
 
 # Dataframe met nodes (= codes van landgebruiksklassen en bijhorende labels)
 
@@ -36,7 +58,7 @@ nodes <- cp1 %>%
 
 # Dataframe met links (= source (van), target (naar) en value (oppervlakte))
 
-links <- sank %>%
+links_pre <- sank %>%
   unite("cp1", c("jaarS","from")) %>%
   unite("cp2", c("jaarT","to")) %>% 
   left_join(nodes, by = c("cp1" = "codetxt")) %>% 
@@ -53,12 +75,15 @@ groups <- sank %>%
   left_join(nodes, by = c("cp1" = "codetxt")) %>% 
   rename(source = code) %>% 
   left_join(nodes, by = c("cp2" = "codetxt")) %>% 
-  rename(target = code) 
+  rename(target = code)
+groups_links <- groups %>%
+  dplyr::select(labelS, source) %>% 
+  distinct(source, .keep_all = TRUE)
 
 # Groepen toewijzen aan nodes en links
 
-links <- links %>% 
-  left_join(select(groups, labelS, source), by = "source") %>% 
+links <- links_pre %>% 
+  left_join(groups_links, by = "source") %>% 
   rename("group" = "labelS")
 
 nodes <- nodes %>% 
@@ -78,14 +103,16 @@ sankeyNetwork(Links = data$links, Nodes = data$nodes, Source = 'source',
               Target = 'target', Value = 'value', NodeID = 'codetxt',
               units = 'ha', colourScale = , fontSize = 12, nodeWidth = 25, nodePadding = 20)
 
-# Poging om de kleuren aan te passen
+# Kleuren voor nodes en links
 
-colors <- 'd3.scaleOrdinal() .domain(["akker", "grasland", "bos"]) .range(["#fff7bc", "#addd8e", "#004529"])'
+colors <- 'd3.scaleOrdinal() .domain(["landbouw", "urbaan", "natuur", "water", "zee"]) 
+.range(["#feb24c", "#a50f15", "#004529", "#6baed6", "#023858"])'
 
 sankeyNetwork(Links = data$links, Nodes = data$nodes, Source = 'source',
               Target = 'target', Value = 'value', NodeID = 'group',
               units = 'ha', fontSize = 12, nodeWidth = 25, nodePadding = 20,
               NodeGroup = "group", LinkGroup = "group",
-              colourScale = colors,
+              fontFamily = "calibri",
+              colourScale = colors, linkOpacity = 0.4,
+              showNodeValues = FALSE,
               iterations = 0)
-
